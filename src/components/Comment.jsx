ce5581203610676cfdbea8dcdd1dfcc6c4cf837c
Noronhajr13 <noronhajf22@gmail.com>
2024-07-29 23:28:09 -0300

    Os motores do React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,19 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-import { ThumbsUp, Trash } from "phosphor-react";
-import { Avatar } from "./Avatar";
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
-export function Comment() {
+  function handleDeleteComment() {
+    onDeleteComment(content); 
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -17,16 +27,16 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
-          <p>Muito bom, parabéns!!</p>
+          <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir<span>50</span>
+            Aplaudir<span>{likeCount}</span>
           </button>
         </footer>
       </div>
